ad28cbba2a55c12d365553c1fe478028e349fc36
hugoalcala <hugo.pascual.alcala@gmail.com>
2025-09-26 10:40:58 +0200

    Empezando a hacer el header hay que terminarlo y mirar el fallo con el logo

---
+++ src/modulos/Header.jsx
@@ -0,0 +1,18 @@
+import "./Header.css";
+import logo from "./assets/Logo/logoEatRest.png"
+function Header(){
+    return(
+        <header className="header">
+            <div className="logo-container">
+                <img src={logo} alt="Eat&Rest logo" className="logo"></img>
+                <h1 className="titulo">EAT & REST</h1>
+            </div>
+            <nav className="barra_navegación">
+                <a href="restaurantes">restaurantes</a>
+                <a href="alojamiento">alojamiento</a>
+                <a href="reservas">reservas</a>
+            </nav>
+        </header>
+    );
+}
+export default Header
